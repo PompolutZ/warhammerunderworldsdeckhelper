8825a6e9c8b030e26b30acf55c1a4b72e794eb8b
Oleh Lutsenko <oleh.lutsenko@tobii.com>
2017-12-14 14:49:22 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Warhammer Underwords Deck Helper</h1>
+        </header>
+        <p className="App-intro">
+          Home page
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/configureStore.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import { loadState, saveState } from './utils/localStorage';
+import { throttle } from 'lodash';
+import appReducers from './reducers/index'
+
+const loggerMiddleware = createLogger();
+
+const configureStore = () => {
+    const persistedStore = loadState();
+    const store = createStore(
+        appReducers, 
+        persistedStore,
+        applyMiddleware(
+            loggerMiddleware
+        ));
+    
+    console.log(store.getState());
+    
+    const unsdub = store.subscribe(throttle(() => {
+      saveState(store.getState());
+      console.log(store.getState());
+    }, 1000));
+    
+    return store;
+}
+
+export default configureStore;
