e7f06e4ee5bb259b6d4aa4b242d08c30f80bb850
Oleh Lutsenko <oleh.lutsenko@tobii.com>
2018-04-08 11:22:49 +0200

    local copy of cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import { Grid, Col, Row, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { Grid, Col, Row, FormGroup, FormControl, ControlLabel, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 import { dbref } from '../utils/firebase';
+import { cardsList } from '../data/index';
 
 const uuidv4 = require('uuid');
 
@@ -13,7 +14,7 @@ export default class Cards extends Component {
       this.state = {
         cards: [],
         error: '',
-        loading: true
+        loading: false
       }
 
       this.onViewChanged = this.onViewChanged.bind(this);
@@ -21,16 +22,20 @@ export default class Cards extends Component {
   
     async componentDidMount() {
       try {
-        const cardsSnapshot = await dbref('/cardslist').once('value');
-        const cardsDb = cardsSnapshot.val();
-        this.setState({loading: false});
+        // const cardsSnapshot = await dbref('/cardslist').once('value');
+        // const cardsDb = cardsSnapshot.val();
+        // this.setState({loading: false});
   
-        console.log('CardsList: ', cardsDb);
+        // console.log('CardsList: ', cardsDb);
         const cards = [];
-        for (let card in cardsDb) {
-          if(cardsDb[card]) {
-            cards.push(<Link className="cardLink" to={`/cards/${card}`} key={uuidv4()}>{`${card}: ${cardsDb[card]}`}</Link>)
-          } 
+        const leadingZeros = ['', '0', '00']
+        for (let card in cardsList) {
+          const img = `01${leadingZeros[3 - card.toString().length]}${card}.png`;
+          cards.push(<Image src={`assets/cards/${img}`} responsive />);
+          // if(cardsList[card]) {
+
+          //   //cards.push(<Link className="cardLink" to={`/cards/${card}`} key={uuidv4()}>{`${card}: ${cardsList[card]}`}</Link>)
+          // } 
         }
   
         this.setState({cards: cards});
